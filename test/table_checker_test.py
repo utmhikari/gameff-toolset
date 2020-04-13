b81facbadc7e765f395aa8b0914b6aed7109dce8
HiKari <361914599@qq.com>
2020-04-13 07:37:16 +0000

    set table check data as external var

--- test/table_checker_test.py
+++ test/table_checker_test.py
@@ -1,16 +1,18 @@
 from lib.table_checker import load_checkers, table_checker
 
+data = {
+    'item': [
+        {'id': 1, 'name': 'haha'},
+        {'id': 2, 'name': 'hehe'},
+        {'id': 3, 'name': ''}
+    ],
+    'good': [
+        {'id': 900001, 'item_id': 2},
+        {'id': 900002, 'item_id': 4}
+    ]
+}
+
 
 if __name__ == '__main__':
     load_checkers('./test/table_checker')
-    table_checker.check({
-        'item': [
-            {'id': 1, 'name': 'haha'},
-            {'id': 2, 'name': 'hehe'},
-            {'id': 3, 'name': ''}
-        ],
-        'good': [
-            {'id': 900001, 'item_id': 2},
-            {'id': 900002, 'item_id': 4}
-        ]
-    })
+    table_checker.check(data)
